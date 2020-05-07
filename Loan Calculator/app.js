b04a6c4299a9eebd58487890c8cae0d74c8c299f
hamzalimouri <limouri.hamza@gmail.com>
2020-05-07 18:36:55 +0000

    Loan Calculator

---
+++ Loan Calculator/app.js
@@ -0,0 +1,59 @@
+// add Event 
+document.getElementById('loan-form').addEventListener('submit', function(evt)
+{
+  document.querySelector('.result').style.display = 'none';
+
+  document.querySelector('.loading').style.display = 'block';
+
+  setTimeout(calculateResult, 2000);
+  
+  evt.preventDefault();
+});
+
+// function 
+
+function calculateResult()
+{
+    // var 
+    const amount = document.getElementById('amount').value;
+    const interest = document.getElementById('interest').value;
+    const years = document.getElementById('year').value;
+
+    const monthlyPayment = document.getElementById('monthlyPayment');
+    const totalInterest = document.getElementById('totalInterest');
+    const totalPayment = document.getElementById('totalPayment');
+
+    const principal = parseFloat(amount);
+    const calculatedInterest = parseFloat(interest) / 100 / 12;
+    const calculatedPayments = parseFloat(years) * 12;
+
+    // Compute monthly payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal*x*calculatedInterest)/(x-1);
+
+    if(isFinite(monthly)) {
+      monthlyPayment.value = monthly.toFixed(2);
+      totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+      totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+      document.querySelector('.result').style.display = 'block';
+    }
+    else
+    {
+      showError();
+    }
+    document.querySelector('.loading').style.display = 'none';
+}
+
+function showError()
+{
+  let div = document.createElement('div');
+  div.className = 'alert alert-danger';
+  div.appendChild(document.createTextNode('Please check your numbers'));
+  document.querySelector('.card').insertBefore(div, document.getElementById('heading'));
+  setTimeout(clearError, 3000);
+}
+
+function clearError()
+{
+  document.querySelector('.alert').remove();
+}
